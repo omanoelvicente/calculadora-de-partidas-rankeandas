9fbf8c8c0eccdf66afdacdbb44de3bc94715c64a
Manoel Vicente <manoelj.vicente@gmail.com>
2024-03-03 23:49:25 +0000

    :sparkles: feat: add javascript file

---
+++ index.js
@@ -0,0 +1,36 @@
+function main() {
+    let winningBalance = calculateWins(30,1);
+    let message = levelPlayer(winningBalance);
+    showMessage(winningBalance, message);
+}
+
+function calculateWins(numberOfWins, numberOfDefeats) {
+     return numberOfWins - numberOfDefeats;
+
+}
+
+function levelPlayer(winningBalance)
+{
+    
+    if (winningBalance <= 10) {
+        message = "Ferro";
+    } else if ((winningBalance > 11) && (winningBalance <= 20)) {
+        message = "Bronze";
+    } else if ((winningBalance > 21) && (winningBalance <= 50)) {
+        message = "Prata";
+    } else if ((winningBalance > 51) && (winningBalance <= 80)) {
+        message = "Ouro"
+    } else if ((winningBalance> 81) && (winningBalance <= 90)) {
+        message = "Diamante";
+    } else if ((winningBalance > 91) && (winningBalance <= 100)) {
+        message = "Lendario";
+    } else {
+        message = "Imortal";
+    }
+    
+    return message;
+}
+
+function showMessage(winningBalance, message  ) {
+    alert(`"O Herói tem de saldo de ${winningBalance} está no nível de ${message}`);
+}
